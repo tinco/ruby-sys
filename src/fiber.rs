aea9a299f67b3296532dc9355d6fb60104448a2f
Tinco Andringa <mail@tinco.nl>
2017-04-24 18:57:32 +0200

    add rb_fiber_yield

--- src/fiber.rs
+++ src/fiber.rs
@@ -4,4 +4,5 @@ extern "C" {
     pub fn rb_fiber_new(function: extern "C" fn(arg: Value, box_ptr: *mut c_void, argc: Argc, argv: *const Value, blockarg: Value) -> Value,
                             data: *mut c_void) -> Value;
     pub fn rb_fiber_resume(fiber: Value, argc: Argc, argv: *const Value) -> Value;
+    pub fn rb_fiber_yield(argc: Argc, argv: *const Value) -> Value;
 }
