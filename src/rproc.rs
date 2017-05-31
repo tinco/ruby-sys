a5e16cfc85bf88882662cabcad98f4b7552d5fd9
Tinco Andringa <mail@tinco.nl>
2017-05-31 18:17:58 +0200

    make rprocs work with closures from the utils

--- src/rproc.rs
+++ src/rproc.rs
@@ -1,4 +1,4 @@
-use types::{Argc, Value};
+use types::{Argc, Value, c_void};
 
 extern "C" {
     pub fn rb_proc_call_with_block(rproc: Value,
@@ -7,5 +7,6 @@ extern "C" {
                                    pass_procval: Value)
                                    -> Value;
 
-    pub fn rb_proc_new(func: extern fn(Value) -> Value, binding: Value) -> Value;
+    pub fn rb_proc_new(function: extern "C" fn(arg: Value, box_ptr: *mut c_void, argc: Argc, argv: *const Value, blockarg: Value) -> Value,
+                            data: *mut c_void) -> Value;
 }
--- tests/rproc_test.rs
+++ tests/rproc_test.rs
@@ -1,25 +1,31 @@
 extern crate ruby_sys;
-use ruby_sys::{vm,rproc, value, fixnum};
+use ruby_sys::{vm,rproc, value, fixnum, util, types};
+use ruby_sys::types::{Argc, c_char, Id, Value, c_void};
+
 
 #[test]
-fn rb_proc_new_works_with_one_argument() {
+fn rb_proc_new_works_with_closure() {
     unsafe { vm::ruby_init() };
 
     let nil = value::Value { value: value::RubySpecialConsts::Nil as usize };
 
-    extern fn test_fn(v: value::Value) -> value::Value {
-        return v;
-    }
+    let run_test = || {
+        unsafe {
+            let val = fixnum::rb_int2inum(3);
+            let test_fn = |arg: Value, argc: Argc, argv: *const Value, blockarg: Value| { unsafe { fixnum::rb_int2inum(3) } };
+            let test_fn_box = util::block_box_ptr_create(test_fn);
+            let rproc = rproc::rb_proc_new(util::rbsys_block_box_ptr_value, test_fn_box);
+            let result = rproc::rb_proc_call_with_block(rproc, 1, vec![val].as_ptr(), nil);
+            assert!(!result.is_nil());
+            let val_result = fixnum::rb_num2int(result);
+            assert!(3 == val_result);
+        }
+    };
 
     unsafe {
-        let val = fixnum::rb_int2inum(3);
-        let rproc = rproc::rb_proc_new(test_fn, nil);
-        let result = rproc::rb_proc_call_with_block(rproc, 1, vec![val].as_ptr(), nil);
-
-        assert!(!result.is_nil());
-        let val_result = fixnum::rb_num2int(result);
-        assert!(3 == val_result);
-    }
-
-    unsafe { vm::ruby_cleanup(0) };
+        let run_test_box = util::closure_box_ptr_create(run_test);
+        let mut state = 0;
+        vm::rb_protect(util::rbsys_closure_box_ptr_void as types::CallbackPtr, run_test_box, &mut state as *mut types::c_int);
+        vm::ruby_cleanup(state)
+    };
 }
